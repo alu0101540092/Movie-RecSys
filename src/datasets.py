d2cb827acb1106f60ac92f77120602c0090bf5fe
Samuel Frías <alu0101540092@ull.edu.es>
2025-11-11 16:39:59 +0000

    Add prompt=False to load_ml100k function

--- src/datasets.py
+++ src/datasets.py
@@ -3,4 +3,4 @@ from surprise import Dataset
 
 def load_ml100k():
     """Carga el dataset MovieLens 100k desde Surprise."""
-    return Dataset.load_builtin("ml-100k")
+    return Dataset.load_builtin("ml-100k", prompt=False)
